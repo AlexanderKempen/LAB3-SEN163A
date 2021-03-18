cfe6b56d198b6f66f982f8cc4e76ed6ad2556dff
AlexanderKempen <Alexander@kempenonline.com>
2021-03-18 17:02:57 +0100

    Fixed loop
    Prints all first 10 of all months of all years

--- Lab3-git.py
+++ Lab3-git.py
@@ -13,7 +13,16 @@ import multiprocessing
 import time
 import concurrent.futures
 
-result = requests.get('https://news.tabularazor.org/')
+start = time.perf_counter()
+
+column_names = ["Name", "Surname", "Year", "Month", "Day","Time","Title"]
+df = pd.DataFrame(columns = column_names)
+
+column_names = ["Name", "Surname", "Year", "Month", "Day","Time","Title"]
+df2 = pd.DataFrame(columns = column_names)
+
+#Initial website newspaper Tabularazor
+tabularazor = 'https://news.tabularazor.org/'
 
 #Indicates that page is accessible if 200 and valid
 def checkStatusSite(site):
@@ -21,69 +30,81 @@ def checkStatusSite(site):
         headers = site.headers
         print('Site is available','\n')
         print('The headers of the site are:', '\n', headers, '\n')
-   # Hashtag
-src = result.content
-soup = BeautifulSoup(src,'lxml')
-#A tag is a hyperlink within the page, creates list of all the links
-yearHyperlinks = soup.find_all('a')
-#List for all the yearlinks
-years = []
-#Accessing all the links, adding to the list, and printing them in order
-for yearHyperlink in yearHyperlinks:
-    years.append(yearHyperlink.attrs['href'])
+
+#Creates connection with a specific site creating soup object
+def createSoupConnection(http):
+    inputUrl = requests.get(http)
+    src = inputUrl.content
+    soup = BeautifulSoup(src, 'lxml')
+    return soup
 
 
-column_names = ["Name", "Surname", "Year", "Month", "Day","Time"]
-df = pd.DataFrame(columns = column_names)
-
-column_names = ["Name", "Surname", "Year", "Month", "Day","Time"]
-df2 = pd.DataFrame(columns = column_names)
-
-
-resultMonths = requests.get('https://news.tabularazor.org/' + years[0])
-src = resultMonths.content
-soups = BeautifulSoup(src,'lxml')
-monthHyperlinks = soups.find_all('a')
-
-months = []
-for monthHyperlink in monthHyperlinks:
-    months.append(monthHyperlink.attrs['href'])
+def findHtmlLink(soupConnection):
+    htmlLinks = []
+    hyperlinks = soupConnection.find_all('a')
+    #Convert to only text part
+    #for hyperlink in hyperlinks:
+    for hyperlink in hyperlinks:
+        htmlLinks.append(hyperlink.attrs['href'])
+    return htmlLinks
     
+
+#Create connection with the site
+soupSite = createSoupConnection(tabularazor)
+
+#Create list for all the article years
+years = findHtmlLink(soupSite)
+
+#Create connection with the month site section
+soupMonth = createSoupConnection(tabularazor+years[0])
+months = findHtmlLink(soupMonth)
+
+
+def getArticleInfo(article):
+    index = len(df.index)
+    soupArticle = createSoupConnection(tabularazor+article)
+    author = soupArticle.find('div', 'author')
+    date = soupArticle.find('div', 'date')
+    times = soupArticle.find('div','time')
+    title = soupArticle.find('h1')
+    titles = title.get_text()
+    timess = times.get_text()
+    authora = author.get_text()
+    data = date.get_text().split('-')
+    name = authora.split()
+    df.loc[index,'Name'] = name[0]
+    df.loc[index, 'Surname'] = name[1]
+    df.loc[index,'Year'] = data[0]
+    df.loc[index,'Month'] = data[1]
+    df.loc[index,'Day'] = data[2]
+    df.loc[index,'Time'] = timess
+    df.loc[index, 'Title'] = titles
+               
+
 def getArticlesOfMonth(month):
-        index = 0
-        resultArticles = requests.get('https://news.tabularazor.org/' + month)
-        src = resultArticles.content
-        soupss = BeautifulSoup(src,'lxml')
-        articleHyperlinks = soupss.find_all('a')
+        soupArticles = createSoupConnection(tabularazor+month)
+        articles = findHtmlLink(soupArticles)
+        for article in articles[:10]:
+            print('i')
+            getArticleInfo(article)
+        return df
+    
+def getMonthsOfYear(year):
+    soupMonth = createSoupConnection(tabularazor+year)
+    months = findHtmlLink(soupMonth)
+    for month in months:
+        getArticlesOfMonth(month)
         
-        articles = []
-        for articleHyperlink in articleHyperlinks:
-            articles.append(articleHyperlink.attrs['href'])
-        
-            for article in articles:
-                resultArticle = requests.get('https://news.tabularazor.org/' + article)
-                src = resultArticle.content
-                soupsss = BeautifulSoup(src,'lxml')
-                author = soupsss.find('div', 'author')
-                date = soupsss.find('div', 'date')
-                times = soupsss.find('div','time')
-                timess = times.get_text()
-                authora = author.get_text()
-                data = date.get_text().split('-')
-                name = authora.split()
-                df.loc[index,'Name'] = name[0]
-                df.loc[index, 'Surname'] = name[1]
-                df.loc[index,'Year'] = data[0]
-                df.loc[index,'Month'] = data[1]
-                df.loc[index,'Day'] = data[2]
-                df.loc[index,'Time'] = timess
-                index = index + 1
-                return df
+    
+
 
 if __name__ == '__main__':
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = executor.map(getArticlesOfMonth, months)
+        results = executor.map(getMonthsOfYear,years)
         for result in results:
             df2 = df2.append(result)
-            print(df2) 
+            #print(df2)
+  
+        
+finish = time.perf_counter()
 
