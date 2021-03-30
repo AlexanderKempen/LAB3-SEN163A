6aa3be201630cd9a6bc2d8c6cdc12bef15652ad9
AlexanderKempen <Alexander@kempenonline.com>
2021-03-30 14:30:28 +0200

    Update Analysis-git.py

--- Analysis/Analysis-git.py
+++ Analysis/Analysis-git.py
@@ -37,11 +37,11 @@ def extractAuthors(df):
     return authors
 
 # Creates the publishing table for all authors and years
-def createWorkingTable(df):
+def createWorkingTable(df, weekType):
     # Creates a list of the authors
     authors = extractAuthors(df)
     # Index as dates, 'D' for all calendar dates. 'B' could be used for all business days.
-    index = pd.date_range(start = '2012/1/1', end = '2019/12/31', freq='D')
+    index = pd.date_range(start = '2012/1/1', end = '2019/12/31', freq=weekType)
     # Authors as columns
     columns = [authors]
     # Create the DataFrame
@@ -49,20 +49,32 @@ def createWorkingTable(df):
     df = df.fillna(0)
     return df
  
-# Fills the publishing table with the number of published article per day for each author
-def fillWorkingTable(df_scrape, df_filled):
-    for i in range(len(df_scrape.index)):
-        date = df_scrape.loc[i,'Date']
-        name = df_scrape.loc[i,'Name']
-        df_filled.loc[[date],[name]] += 1
-    df_filled.to_csv("Publising schedule.csv")
+# Fills the publishing table with the number of published article per weekday for each author
+def fillWorkingTable(df_scrape, df_filled, weekType):
+    if weekType == 'D':                             #Check if weektype is set for only business days
+            for i in range(len(df_scrape.index)):
+                date = df_scrape.loc[i,'Date']
+                name = df_scrape.loc[i,'Name']
+                df_filled.loc[[date],[name]] += 1
+            df_filled.to_csv("Publising schedule weeks.csv")
+            return df_filled
+
+    else:
+        for i in range(len(df_scrape.index)):
+            date = df_scrape.loc[i,'Date']
+            name = df_scrape.loc[i,'Name']
+            if date in df_filled.index:
+                df_filled.loc[[date],[name]] += 1
+    df_filled.to_csv("Publising schedule business days.csv")
     return df_filled
+
     
+weekType = "D"
 # Creates the empty publishing table
-workingTable = createWorkingTable(df)
+workingTable = createWorkingTable(df, weekType)
 
 # Creates the publishing table with the number of published article per day for each author for the history of Tabularazor Inc.
-filledWorkingTable = fillWorkingTable(df, workingTable)
+filledWorkingTable = fillWorkingTable(df, workingTable, weekType)
 
 
 # End the timer
